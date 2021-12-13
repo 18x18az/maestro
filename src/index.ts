3608c6cffae97ec030548d5da24a239a19968ad5
Alec Miller <alecmichaelmiller@gmail.com>
2021-12-12 19:42:13 -0700

    added logging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { record, getNextId, LogType, IMetadata } from "./utils/log"
+
+const id = getNextId();
+const meta: IMetadata = {
+    id
+}
+
+function doTheThing(metadata: IMetadata){
+    record(metadata, LogType.LOG, "hello world");
+}
+
+doTheThing(meta);
+
---
+++ src/utils/log.ts
@@ -0,0 +1,23 @@
+let id = 0;
+
+type ILogId = number;
+
+
+export enum LogType {
+    ERROR = "ERROR",
+    LOG = "LOG"
+}
+
+
+export interface IMetadata{
+    id: ILogId
+}
+
+export function getNextId(): ILogId{
+    return id++;
+}
+
+export function record(metadata: IMetadata, type: LogType, message: string){
+    const payload = {message, ...metadata, type};
+    console.log(`${type}: ${message} - ${JSON.stringify(metadata)}`);
+}
