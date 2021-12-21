e502dda9255b8ee63ffe8a92966df62f3aa34cfd
Josh T <joshliam09@gmail.com>
2021-12-20 23:41:23 -0700

    obs websocket mwe

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { TeamId } from "@18x18az/rosetta";
 import { record, getNextId, LogType, IMetadata } from "./utils/log"
 import { AllianceSelection } from "./state/alliance"
+import ObsWebSocket from "obs-websocket-js";
 
 const id = getNextId();
 const meta: IMetadata = {
@@ -14,6 +15,40 @@ function doTheThing(metadata: IMetadata){
 
 doTheThing(meta);
 
+// create websocket
+const obs = new ObsWebSocket();
+obs.connect({address: 'localhost:4444'}).then(() => {
+    console.log(`Success! We're connected & authenticated.`);
+
+    return obs.send('GetSceneList');
+}).then(data => {
+    console.log(`${data.scenes.length} Available Scenes!`);
+
+    data.scenes.forEach(scene => {
+        if (scene.name !== data["current-scene"]) {
+            console.log(`Found a different scene! Switching to Scene: ${scene.name}`);
+
+            obs.send('SetCurrentScene', {
+                'scene-name': scene.name
+            });
+        }
+    });
+});
+
+
+const callback = (data: any) => {
+	console.log(data);
+};
+
+// The following are additional supported events.
+obs.on('ConnectionOpened', (data) => callback(data));
+obs.on('ConnectionClosed', (data) => callback(data));
+obs.on('AuthenticationSuccess', (data) => callback(data));
+obs.on('AuthenticationFailure', (data) => callback(data));
+
+
+
+/* // alliance selection test
 let stdin = process.openStdin();
 
 let teams: Array<string> = [];
@@ -22,12 +57,9 @@ for(let i = 1; i <= 38; i++){
 }
 let als: AllianceSelection = new AllianceSelection(teams, meta);
 
-stdin.addListener("data", function(d) {
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that  
-    // with toString() and then substring() 
 
-    console.log("you entered: [" + d.toString().trim() + "]");
+stdin.addListener("data", function(d) {
+console.log("you entered: [" + d.toString().trim() + "]");
     switch(d.toString().trim()[0]){
         case "p":
             als.pick(d.toString().trim().substring(1), meta);
@@ -42,7 +74,5 @@ stdin.addListener("data", function(d) {
             als.undo(meta);
             break;
     }
-        
-    
-
 });
+*/
