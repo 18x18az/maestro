7c36164e264527d464b36f6775a3381b383caa19
Alec Miller <alecmichaelmiller@gmail.com>
2022-02-28 23:33:12 -0700

    show complete coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ]
 };
