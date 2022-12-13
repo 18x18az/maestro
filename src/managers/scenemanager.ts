74584315288e35bbeb9536032dc45587c75cb732
Joshua Tenorio <joshliam09@gmail.com>
2022-12-13 13:28:07 -0700

    WIP

--- src/managers/scenemanager.ts
+++ src/managers/scenemanager.ts
@@ -1,14 +1,17 @@
 import { privateEncrypt } from "crypto";
 import ObsWebSocket from "obs-websocket-js";
 import { record, IMetadata, LogType } from "../utils/log";
+import { config } from "dotenv";
 
+config();
 /**
  * Connects to an obs-websocket and controls scene switching
  */
 export class SceneManager {
 
-    wsAddr: string = "owowo"
+    wsAddr: string = "owowo";
     obs: ObsWebSocket = new ObsWebSocket();
+    isManual: boolean = false;
 
     constructor(){
         this.connect();
@@ -19,10 +22,14 @@ export class SceneManager {
             const {
               obsWebSocketVersion,
               negotiatedRpcVersion
-            } = await this.obs.connect('ws://localhost:4444', 'password');
-            console.log(`Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`)
+            } = await this.obs.connect(`ws://localhost:${process.env.OBS_WS_PORT as string}`);
+            console.log(`Connected to OBS server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`)
         } catch (error: any) {
             console.error('Failed to connect', error.code, error.message);
         }
+    }
+
+    async setField(){
+        
     }
 }
