86e55fea78b732767675db94f2b3bf931e9f1b8b
Joshua Tenorio <joshliam09@gmail.com>
2022-12-19 22:53:06 -0700

    chore: update rosetta

--- src/handlers/obs.ts
+++ src/handlers/obs.ts
@@ -1,13 +1,6 @@
 import { Studio } from "../managers/obs";
 import { IMetadata } from "../utils/log";
-import { IMessage, MESSAGE_TYPE } from "@18x18az/rosetta";
-
-// TODO: move to rosetta
-interface IOBSConfig {
-    setManual: boolean,
-    isConnected: boolean,
-    attemptReconnect: boolean
-}
+import { IMessage, MESSAGE_TYPE, IOBSConfig } from "@18x18az/rosetta";
 
 export async function postOBSHandler(metadata: IMetadata, message: IMessage) {
     if ((message.payload as IOBSConfig).setManual) {
