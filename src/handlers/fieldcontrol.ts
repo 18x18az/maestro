925541baa31d0f704f5b653401aba49b98f1b6e1
Joshua Tenorio <joshliam09@gmail.com>
2022-12-28 22:56:46 -0700

    random things for field control

--- src/handlers/fieldcontrol.ts
+++ src/handlers/fieldcontrol.ts
@@ -2,6 +2,15 @@ import { IMessage, MESSAGE_TYPE } from "@18x18az/rosetta";
 import { IMetadata, LogType, record } from "../utils/log";
 import { broadcast } from "../utils/wss";
 
+export enum FIELD_CONTROL_STATE {
+    IN_MATCH = "START",
+    BETWEEN_MATCHES = "RESUME"
+}
+
+export interface IFieldControlState {
+    state: FIELD_CONTROL_STATE
+}
+
 export function postFieldControlHandler(metadata: IMetadata, message: IMessage) {
     record(metadata, LogType.LOG, 'field control owo')
     record(metadata, LogType.DATA, JSON.stringify(message.payload));
@@ -10,4 +19,8 @@ export function postFieldControlHandler(metadata: IMetadata, message: IMessage) 
         path: ['fieldcontrol'],
         payload: message.payload
     });
+}
+
+export function getFieldControlHandler(metadata: IMetadata, message: IMessage) {
+
 }
