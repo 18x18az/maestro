3f21ef2e7f0ea6a316bf39c45126fed43be417d5
Alec Miller <alecmichaelmiller@gmail.com>
2023-01-02 21:14:42 -0700

    feat: improved award handling

--- src/handlers/awards.ts
+++ src/handlers/awards.ts
@@ -1,10 +1,11 @@
-import { IMessage, MESSAGE_TYPE, IAward, IAwards, COMPETITION_STAGE } from "@18x18az/rosetta";
+import { IMessage, MESSAGE_TYPE, IAward, IAwards, COMPETITION_STAGE, DISPLAY_STATE } from "@18x18az/rosetta";
 import { IMetadata, LogType, record } from "../utils/log";
 import { broadcast } from "../utils/wss";
+import { setDisplayState } from "./display";
 import { setCompetitionStage } from "./stage";
 
 let awards: IAwards;
-let selectedAward: IAward;
+let selectedAward: IAward | null;
 let isPushed: boolean;
 
 function refreshAwards(metadata: IMetadata) {
@@ -15,8 +16,12 @@ function refreshAwards(metadata: IMetadata) {
     });
 }
 
-function getSelectedAward(): IAward{
-    if(isPushed){
+function getSelectedAward(): IAward | null {
+    if (!selectedAward) {
+        return null
+    }
+
+    if (isPushed) {
         return selectedAward;
     } else {
         let modified = JSON.parse(JSON.stringify(selectedAward)) as IAward;
@@ -25,10 +30,35 @@ function getSelectedAward(): IAward{
     }
 }
 
+function selectAward(metadata: IMetadata, index: number) {
+    const awardToUse = JSON.parse(JSON.stringify(awards[index]));
+    record(metadata, LogType.LOG, 'Award selected');
+    record(metadata, LogType.LOG, `Selected ${awardToUse.name}`);
+    selectedAward = awardToUse;
+    isPushed = false;
+    setDisplayState(metadata, DISPLAY_STATE.NONE);
+    setCompetitionStage(metadata, COMPETITION_STAGE.AWARDS);
+    broadcast(metadata, {
+        type: MESSAGE_TYPE.POST,
+        path: ["awards", "selected"],
+        payload: getSelectedAward()
+    });
+}
+
 export function postAwardsHandler(metadata: IMetadata, message: IMessage) {
-    if(message.payload === null){
-        if(message.path[1] === "push") {
+    if (message.payload === null) {
+        if (message.path[1] === "push") {
+            record(metadata, LogType.LOG, 'Award pushed to display');
             isPushed = true;
+            broadcast(metadata, {
+                type: MESSAGE_TYPE.POST,
+                path: ["awards", "selected"],
+                payload: getSelectedAward()
+            });
+        } else if (message.path[1] === "selected") {
+            record(metadata, LogType.LOG, 'Deselecting award');
+            selectedAward = null;
+            isPushed = false;
             broadcast(metadata, {
                 type: MESSAGE_TYPE.POST,
                 path: ["awards", "selected"],
@@ -37,18 +67,9 @@ export function postAwardsHandler(metadata: IMetadata, message: IMessage) {
         } else {
             refreshAwards(metadata);
         }
-    } else if(message.path[1] === "selected"){
-        isPushed = false;
-        record(metadata, LogType.LOG, 'Award selected');
+    } else if (message.path[1] === "selected") {
         const messageIndex = parseInt(message.payload)
-        selectedAward = awards[messageIndex];
-        record(metadata, LogType.LOG, `Selected ${selectedAward.name}`);
-        setCompetitionStage(metadata, COMPETITION_STAGE.AWARDS);
-        broadcast(metadata, {
-            type: MESSAGE_TYPE.POST,
-            path: ["awards", "selected"],
-            payload: getSelectedAward()
-        });
+        selectAward(metadata, messageIndex);
     } else {
         record(metadata, LogType.LOG, "awards updated");
         awards = message.payload;
@@ -60,9 +81,9 @@ export function postAwardsHandler(metadata: IMetadata, message: IMessage) {
     }
 }
 
-export function getAwardsHandler(meta: IMetadata): IMessage | null{
+export function getAwardsHandler(meta: IMetadata): IMessage | null {
     record(meta, LogType.LOG, "Selected award requested");
-    if(selectedAward){
+    if (selectedAward) {
         return {
             type: MESSAGE_TYPE.POST,
             path: ['awards', 'selected'],
