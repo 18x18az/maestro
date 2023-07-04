976611f100679d98e094ef53ceba140ac6c87863
Alec Miller <alecmichaelmiller@gmail.com>
2023-07-04 15:53:36 -0700

    feat(teamInfo): raw team list

--- src/modules/TeamInfo/index.ts
+++ src/modules/TeamInfo/index.ts
@@ -1,5 +1,5 @@
-import { AUTH_TYPE, PathComponent, TeamInfo, TeamInfoTable } from '@18x18az/rosetta'
-import { InputProcessor, SingleModule, addSimpleSingleBroadcast, addSimpleSingleDatabase, addSimpleSinglePostHandler } from '../../components'
+import { AUTH_TYPE, MessagePath, PathComponent, TeamInfo, TeamInfoTable } from '@18x18az/rosetta'
+import { BroadcastBuilder, InputProcessor, SingleModule, addBroadcastOutput, addSimpleSingleBroadcast, addSimpleSingleDatabase, addSimpleSinglePostHandler } from '../../components'
 
 const processor: InputProcessor<TeamInfoTable> = (input, current) => {
   const teams = input.get(PathComponent.TEAM_INFO) as TeamInfo[]
@@ -14,11 +14,19 @@ const processor: InputProcessor<TeamInfoTable> = (input, current) => {
   }
 }
 
+const teamListBroadcaster: BroadcastBuilder<TeamInfoTable> = (identifier, value) => {
+  const topic: MessagePath = [[], PathComponent.TEAM_LIST]
+  const payload = Object.keys(value)
+
+  return [topic, payload]
+}
+
 export async function setupTeamInfo (): Promise<void> {
   const module = new SingleModule(PathComponent.TEAM_INFO, processor)
 
   addSimpleSinglePostHandler(module, PathComponent.TEAM_INFO, AUTH_TYPE.SERVICE)
   addSimpleSingleBroadcast(module, PathComponent.TEAM_INFO)
+  addBroadcastOutput(module, teamListBroadcaster)
 
   await addSimpleSingleDatabase(module, undefined)
   console.log('Team Info module loaded')
