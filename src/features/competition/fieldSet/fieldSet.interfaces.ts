9eef1b15b814d5828cf43e4c2a8e3da1c2555a8a
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-30 19:00:56 -0700

    lint fix

--- src/features/competition/fieldSet/fieldSet.interfaces.ts
+++ src/features/competition/fieldSet/fieldSet.interfaces.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger'
 
 export class FieldState {
-  @ApiProperty({ description: 'The ID of the active field in the fieldset', example: '1'})
-  currentField: string | null
+  @ApiProperty({ description: 'The ID of the active field in the fieldset', example: '1' })
+    currentField: string | null
 }
