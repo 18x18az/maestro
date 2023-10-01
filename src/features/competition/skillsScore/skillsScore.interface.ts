9ecb5fb28af91582c0f55a0af5afbd05e470dd94
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-30 19:40:27 -0700

    lint fix

--- src/features/competition/skillsScore/skillsScore.interface.ts
+++ src/features/competition/skillsScore/skillsScore.interface.ts
@@ -9,16 +9,16 @@ enum ELEVATION {
 }
 
 export class SkillsScore {
-  @ApiProperty({ description: "Number of balls in the red goal", example: 3 })
+  @ApiProperty({ description: 'Number of balls in the red goal', example: 3 })
     goalTriballs: number
 
-  @ApiProperty({ description: "Number of balls in the red offensive zone", example: 3 })
+  @ApiProperty({ description: 'Number of balls in the red offensive zone', example: 3 })
     zoneTriballs: number
 
-  @ApiProperty({ description: "Number of red alliance triballs in either goal", example: 1 })
+  @ApiProperty({ description: 'Number of red alliance triballs in either goal', example: 1 })
     allianceTriballsInGoal: number
 
-  @ApiProperty({ description: "Number of red alliance triballs in either offensive zone", example: 1 })
+  @ApiProperty({ description: 'Number of red alliance triballs in either offensive zone', example: 1 })
     allianceTriballsInZone: number
 
   @ApiProperty({ description: 'Elevation level of the robot', example: ELEVATION.B_D, enum: ELEVATION })
