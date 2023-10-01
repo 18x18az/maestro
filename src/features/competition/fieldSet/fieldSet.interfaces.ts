1815507dd494e185eb335d7357db9d8a37ff6fdb
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-30 18:48:37 -0700

    feat: initial fieldset control

---
+++ src/features/competition/fieldSet/fieldSet.interfaces.ts
@@ -0,0 +1,3 @@
+export interface FieldState {
+  currentField: string | null
+}
---
+++ src/features/competition/fieldSet/fieldSet.publisher.ts
@@ -0,0 +1,20 @@
+import { Publisher } from '@alecmmiller/nestjs-client-generator'
+import { Injectable } from '@nestjs/common'
+import { PublishService } from 'src/utils/publish/publish.service'
+import { FieldState } from './fieldSet.interfaces'
+import { Payload } from 'pigeon-mqtt-nest'
+
+function makeFieldSetTopic (uuid: string): string {
+  return `fieldSet/${uuid}`
+}
+
+@Injectable()
+export class FieldSetPublisher {
+  constructor (private readonly publisher: PublishService) {}
+
+  @Publisher('fieldSet/:uuid')
+  async publishFieldSet (uuid: string, @Payload({}) state: FieldState): Promise<void> {
+    const topic = makeFieldSetTopic(uuid)
+    await this.publisher.broadcast(topic, state)
+  }
+}
