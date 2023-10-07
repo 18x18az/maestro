aeafb0fd3eaa27e26d5974c8496e010447c07147
Alec Miller <alecmichaelmiller@gmail.com>
2023-10-06 17:01:02 -0700

    chore: add test mode cofig

--- src/features/mock/mock.controller.ts
+++ src/features/mock/mock.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, MethodNotAllowedException, Post } from '@nestjs/common'
 import { MockService } from './mock.service'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('mock')
 export class MockController {
-  constructor (private readonly mockService: MockService) {}
+  constructor (private readonly mockService: MockService, private readonly config: ConfigService) {}
   @Post('generate')
   async generateMockData (): Promise<void> {
+    if (this.config.get('TEST_MODE') !== 'true') {
+      throw new MethodNotAllowedException()
+    }
     await this.mockService.generate()
   }
 }
