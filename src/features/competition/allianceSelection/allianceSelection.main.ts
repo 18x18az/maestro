68da45b1ff3de4857a5b49af2e2bdbfb20d17a53
Sylvie <me@sylvie.fyi>
2023-10-17 18:13:10 -0400

    changes based on alec's feedback

--- src/features/competition/allianceSelection/allianceSelection.main.ts
+++ src/features/competition/allianceSelection/allianceSelection.main.ts
@@ -1,9 +1,11 @@
+type Team = string
+type Teams = Array<Team>
 
 enum ActionType {
-    Pick = 1,
-    Accept = 2,
-    Decline = 3,
-    NoShow = 4
+    Pick = "PICK",
+    Accept = "ACCEPT",
+    Decline = "DECLINE",
+    NoShow = "NOSHOW"
 }
 
 interface RankingsOperation {
@@ -11,92 +13,124 @@ interface RankingsOperation {
     target?: string
 }
 
+interface Alliance {
+    captain: Team,
+    picked?: Team
+}
+
 interface RankingState {
-    eligible: Array<string>,
-    rankings: Array<string>,
-    alliances: Array<Array<string>>,
-    picking: string,
-    picked: string,
-    ineligible: Array<string>,
+    eligible: Teams,
+    rankings: Teams,
+    alliances: Array<Alliance>,
+    picking: Team,
+    picked?: Team,
+    ineligible: Teams,
     allowed_actions: Array<ActionType>
 }
 
+
+
 class AllianceSelection{
-    _rankings: Array<string>
-    _state: RankingState
-    _operations: Array<RankingsOperation>
-    constructor(rankings: Array<string>){
-        this._state = <RankingState>{}
-        this._rankings = rankings
-        this._operations = new Array<RankingsOperation>()
+    private rankings: Teams
+    private state: RankingState
+    private operations: Array<RankingsOperation>
+    constructor(rankings: Teams){
+        this.rankings = rankings
+        this.operations = new Array<RankingsOperation>()
         this.resetState()
     }
     resetState(): void{
-        this._state.eligible = this._rankings.slice(1)
-        this._state.rankings = this._rankings.slice(1)
-        this._state.alliances = new Array<Array<string>>()
-        this._state.picking = this._rankings[0]
-        this._state.ineligible = new Array<string>()
-        this._state.picked = ""
-        this._state.allowed_actions = [ActionType.Pick, ActionType.NoShow]
+        this.state = {
+            eligible: this.rankings.slice(1),
+            rankings: this.rankings.slice(1),
+            alliances: [],
+            picking: this.rankings[0],
+            ineligible: [],
+            picked: undefined,
+            allowed_actions: [ActionType.Pick, ActionType.NoShow]
+        }
+    }
+    clearPicked(): void{
+        this.state.picked = undefined
+        this.state.allowed_actions = [ActionType.Pick, ActionType.NoShow]
+
+    }
+    doAccept(): void {
+        this.state.alliances.push({captain: this.state.picking, picked: this.state.picked})
+        this.state.eligible = this.state.eligible.filter(meow => meow != this.state.picked)
+        this.state.rankings = this.state.rankings.filter(meow => meow != this.state.picked)
+        this.state.eligible = this.state.eligible.filter(meow => meow != this.state.picking)
+        this.state.rankings = this.state.rankings.filter(meow => meow != this.state.picking)
+        this.clearPicked()
+        this.state.picking = this.state.rankings[0]
+    }
+    doDecline(): void {
+        this.state.eligible = this.state.eligible.filter(meow => meow != this.state.picked)
+        if(this.state.picked !== undefined){
+            this.state.ineligible.push(this.state.picked)
+        }
+        this.clearPicked()
+    }
+    doPick(target: Team): void {
+        if(!this.state.eligible.includes(target)){
+            throw Error("team not eligible")
+        }
+        this.state.picked = target 
+        this.state.allowed_actions = [ActionType.Accept, ActionType.Decline]
+    }
+    doNoShow(): void {
+        this.state.eligible = this.state.eligible.filter(meow => meow != this.state.picking)
+        this.state.rankings = this.state.rankings.filter(meow => meow != this.state.picking)
+        this.state.ineligible.push(this.state.picking)
+        this.state.picking = this.state.rankings[0]
+        this.clearPicked()
     }
     applyOperation(operation: RankingsOperation): void{
         /**
          * changes the state of the internal alliance selection variable based on an instruction given
          */
-        if(!this._state.allowed_actions.includes(operation.action)){
+        if(!this.state.allowed_actions.includes(operation.action)){
             throw Error("action not allowed") // would a specific type of error be better?
         }
-        if(operation.action == ActionType.Pick){
-            if(!this._state.eligible.includes(operation.target as string)){
-                throw Error("team not eligible")
+        switch(operation.action) {
+            case ActionType.Pick: {
+                if (operation.target === undefined) {
+                    throw Error("target not defined")
+                 }
+                this.doPick(operation.target)
+                break
             }
-            this._state.picked = operation.target as string // typescript is fucking annoying sometimes
-            this._state.allowed_actions = [ActionType.Accept, ActionType.Decline]
-        }
-        else if(operation.action == ActionType.Accept){
-            this._state.alliances.push([this._state.picking, this._state.picked])
-            this._state.eligible = this._state.eligible.filter(meow => meow != this._state.picked)
-            this._state.rankings = this._state.rankings.filter(meow => meow != this._state.picked)
-            this._state.eligible = this._state.eligible.filter(meow => meow != this._state.picking)
-            this._state.rankings = this._state.rankings.filter(meow => meow != this._state.picking)
-            this._state.picked = ""
-            this._state.picking = this._state.rankings[0]
-            this._state.allowed_actions = [ActionType.Pick, ActionType.NoShow]
-        }
-        else if(operation.action == ActionType.Decline){
-            this._state.eligible = this._state.eligible.filter(meow => meow != this._state.picked)
-            this._state.ineligible.push(this._state.picked)
-            this._state.picked = ""
-            this._state.allowed_actions = [ActionType.Pick, ActionType.NoShow]
-        }
-        else if(operation.action == ActionType.NoShow){
-            this._state.eligible = this._state.eligible.filter(meow => meow != this._state.picking)
-            this._state.rankings = this._state.rankings.filter(meow => meow != this._state.picking)
-            this._state.ineligible.push(this._state.picking)
-            this._state.picking = this._state.rankings[0]
-            this._state.allowed_actions = [ActionType.Pick, ActionType.NoShow]
-        }
-        else {
-            throw Error("Unknown action")
+            case ActionType.Accept: {
+                this.doAccept()
+                break
+            }
+            case ActionType.Decline: {
+                this.doDecline()
+                break
+            }
+            case ActionType.NoShow: {
+                this.doNoShow()
+                break
+            }
+            default: {
+                throw Error("Unknown action")
+            }
         }
     }
     addOperation(operation : RankingsOperation): void{
         /**
          * adds an operation to the history, and also applies it 
          */
-        this._operations.push(operation)
+        this.operations.push(operation)
         this.applyOperation(operation)
     }
     undo(){
         /**
          * removes the previous operation from the history, and rebuilds the state
          */
-        this._operations.pop()
+        this.operations.pop()
         this.resetState()
-        for (const mrrp of this._operations) {
-            this.applyOperation(mrrp)
-        }
+        this.operations.forEach(mrrp => {this.applyOperation(mrrp)})
         return {success: true}
     }
     buildState(){
@@ -104,16 +138,14 @@ class AllianceSelection{
          * rebuilds the state without making changes. ideally this should never change anything and shouldn't be used
          */
         this.resetState()
-        for (const mrrp of this._operations) {
-            this.applyOperation(mrrp)
-        }
+        this.operations.forEach(mrrp => {this.applyOperation(mrrp)})
         return {success: true}
     }
     clearAllianceSelection(){
         /**
          * resets all of alliance selection. make sure to add an endpoint for this so when i hack your network it will be funny!
          */
-        this._operations = new Array<RankingsOperation>()
+        this.operations = new Array<RankingsOperation>()
         this.resetState()
         return {success: true}
     }
@@ -121,10 +153,10 @@ class AllianceSelection{
         /**
          * instruct the currently picking team to pick the team specifed in the argument
          */
-        if(!this._state.allowed_actions.includes(ActionType.Pick)){
+        if(!this.state.allowed_actions.includes(ActionType.Pick)){
             return {success: false, error: "picking not allowed at this time"}
         }
-        if(!this._state.eligible.includes(team)){
+        if(!this.state.eligible.includes(team)){
             return {success: false, error: "picked team isn't eligible"}
         }
         this.addOperation({
@@ -137,7 +169,7 @@ class AllianceSelection{
         /**
          * have team accept offer
          */
-        if(!this._state.allowed_actions.includes(ActionType.Accept)){
+        if(!this.state.allowed_actions.includes(ActionType.Accept)){
             return {success: false, error: "accepting not allowed at this time"}
         }
         this.addOperation({
@@ -149,7 +181,7 @@ class AllianceSelection{
         /**
          * have team decline offer
          */
-        if(!this._state.allowed_actions.includes(ActionType.Decline)){
+        if(!this.state.allowed_actions.includes(ActionType.Decline)){
             return {success: false, error: "declining not allowed at this time"}
         }
         this.addOperation({
@@ -161,7 +193,7 @@ class AllianceSelection{
         /**
          * call this function if a picking team doesnt show up
          */
-        if(!this._state.allowed_actions.includes(ActionType.NoShow)){
+        if(!this.state.allowed_actions.includes(ActionType.NoShow)){
             return {success: false, error: "no-showing not allowed at this time"}
         }
         this.addOperation({
@@ -169,16 +201,72 @@ class AllianceSelection{
         })
         return {success: true}
     }
-    getCurrentState() : Object{
+    getCurrentState() : RankingState{
         /**
          *  returns the entire current state
          */
-        return this._state
+        return this.state
     }
 }
 
-/* Example
-let a = new AllianceSelection(["5090X", "6030J", "99067B", "127A", "2114V", "PYRO", "RIT3"])
+class AllianceSelectionService{
+    private allianceSelection: AllianceSelection
+    private allianceSelectionRunning: Boolean
+    private savedState: RankingState
+    constructor() {
+        this.allianceSelectionRunning = false
+    }
+    startAllianceSelection(rankings: Teams){
+        this.allianceSelection = new AllianceSelection(rankings)
+        this.allianceSelectionRunning = true
+    }
+    stopAllianceSelection(){
+        this.throwErrorIfNotRunning()
+        this.savedState = this.allianceSelection.getCurrentState()
+        this.allianceSelectionRunning = false
+    }
+    throwErrorIfNotRunning(){
+        if(this.allianceSelection === undefined || this.allianceSelectionRunning == false){
+            throw Error("alliance selection not running")
+        }
+    }
+    pick(target: Team){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.pick(target)
+    }
+    decline(){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.decline()
+    }
+    accept(){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.accept()
+    }
+    noShow(){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.noShow()
+    }
+    undo(){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.undo()
+    }
+    buildState(){
+        this.throwErrorIfNotRunning()
+        return this.allianceSelection.buildState()
+    }
+    getCurrentState(){
+        if(this.allianceSelectionRunning == true){
+            return this.allianceSelection.getCurrentState()
+        }
+        else{
+            return this.savedState
+        }
+    }
+}
+
+/*
+let a = new AllianceSelectionService()
+a.startAllianceSelection(["5090X", "6030J", "99067B", "127A", "2114V", "PYRO", "RIT3"])
 a.noShow()
 a.pick("127A")
 a.decline()
