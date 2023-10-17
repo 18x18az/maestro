af6898aa7402933796d5391511e19f2bdc251c55
Alec Miller <alec@18x18az.org>
2023-10-17 16:47:35 -0700

    feat: correctly broadcast stage

--- src/features/stage/stage.interface.ts
+++ src/features/stage/stage.interface.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export enum EVENT_STAGE {
   SETUP = 'SETUP',
   CHECKIN = 'CHECKIN',
   TEARDOWN = 'TEARDOWN',
   EVENT = 'EVENT'
 }
+
+export class EventStage {
+  @ApiProperty({ enum: EVENT_STAGE, description: 'The current stage of the event', example: EVENT_STAGE.SETUP })
+    stage: EVENT_STAGE
+}
+
+export const EVENT_STAGE_KEY = 'eventStage'
