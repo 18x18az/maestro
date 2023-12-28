0f9da8bb4635945013470bcc34c4678de1bdc249
Alec Miller <alecmichaelmiller@gmail.com>
2023-12-28 10:40:54 -0700

    removed field control query

--- src/features/field-control/field-control.resolver.ts
+++ src/features/field-control/field-control.resolver.ts
@@ -1,22 +1,14 @@
-import { Field, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { Field, Parent, ResolveField, Resolver } from '@nestjs/graphql'
 import { FieldControl } from './field-control.object'
-import { FieldControlService } from './field-control.service'
 import { FieldControlStatus } from './field-control.interface'
 import { FieldService } from '../field/field.service'
 import { FieldEntity } from '../field/field.entity'
-import { FieldControlModel } from './field-control.model'
 
 @Resolver(of => FieldControl)
 export class FieldControlResolver {
   constructor (
-    private readonly fieldControlService: FieldControlService,
     private readonly fieldService: FieldService
   ) {}
-
-  @Query(() => [FieldControl])
-  fieldControls (): FieldControlModel[] {
-    return this.fieldControlService.getFieldControls()
-  }
 
   @ResolveField()
   async isRunning (@Parent() field: FieldControlStatus): Promise<boolean> {
