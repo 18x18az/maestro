fa41cefbbb47094453de334e93b625b6b7313f3a
Alec Miller <alecmichaelmiller@gmail.com>
2024-01-21 18:50:16 -0700

    end early handler

--- src/features/field-control/field-control.resolver.ts
+++ src/features/field-control/field-control.resolver.ts
@@ -6,12 +6,14 @@ import { FieldEntity } from '../field/field.entity'
 import { FieldControlModel } from './field-control.model'
 import { StartFieldEvent } from './start-field.event'
 import { PubSub } from 'mercurius'
+import { StopFieldEvent } from './stop-field.event'
 
 @Resolver(() => FieldControl)
 export class FieldControlResolver {
   constructor (
     private readonly fieldService: FieldService,
-    private readonly startEvent: StartFieldEvent
+    private readonly startEvent: StartFieldEvent,
+    private readonly stopEvent: StopFieldEvent
   ) {}
 
   @ResolveField()
@@ -30,6 +32,16 @@ export class FieldControlResolver {
       @Context('pubsub') pubSub: PubSub
   ): Promise<FieldControlModel> {
     const result = await this.startEvent.execute({ fieldId })
+    pubSub.publish({ topic: 'fieldControl', payload: { fieldControl: result._control } })
+    return result._control
+  }
+
+  @Mutation(() => FieldControl)
+  async stopField (
+    @Args({ type: () => Int, name: 'fieldId' }) fieldId: number,
+      @Context('pubsub') pubSub: PubSub
+  ): Promise<FieldControlModel> {
+    const result = await this.stopEvent.execute({ fieldId })
     pubSub.publish({ topic: 'fieldControl', payload: { fieldControl: result._control } })
     return result._control
   }
